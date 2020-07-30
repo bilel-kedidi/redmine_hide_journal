89d9411645176235eecebbb5fc867b598423a6cc
mobike <bilel.kedidi@gmail.com>
2020-07-30 21:57:28 +0100

    fix

--- db/migrate/001_create_add_is_hidden_to_journals.rb
+++ db/migrate/001_create_add_is_hidden_to_journals.rb
@@ -1,4 +1,4 @@
-class CreateAddIsHiddenToJournals < ActiveRecord::Migration
+class CreateAddIsHiddenToJournals < Rails.version < '5.1' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def change
     add_column :journals, :is_hidden, :boolean, default: false
   end
